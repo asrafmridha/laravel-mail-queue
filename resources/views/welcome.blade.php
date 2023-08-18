3a11e7f4c00f1de90c0f889e88d3598347f10d5b
asraf-mridha <asrafmridha@gmail.com>
2023-08-18 23:08:25 +0600

    setup project

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>Email Queue</title>
+  </head>
+  <body>
+    <div class="container mt-5">
+        <a href="" class="btn btn-success">ADD NEW</a>
+        <table class="table">
+            <thead>
+                <tr>
+                <th scope="col">#Sl</th>
+                <th scope="col">Title</th>
+                <th scope="col">Image</th>
+                <th scope="col">Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+
+  </body>
+</html>
